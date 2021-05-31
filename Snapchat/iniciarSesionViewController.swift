76c55a2489c0fe3b249a12a4b0c2135276cf370f
Javier Flores Càrdenas <javier.flores@tecsup.edu.pe>
2021-05-30 23:07:29 -0700

    Tarea finalizada - Integración de autenticación con Google

--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
-class iniciarSesionViewController: UIViewController {
+class iniciarSesionViewController: UIViewController, GIDSignInDelegate {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.delegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -24,13 +27,29 @@ class iniciarSesionViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(user,error) in
             print("Intentando Iniciar Sesion")
             if error != nil{
-                print("Se presentó el siguiente error: \(error)")
+                print("Se presentó el siguiente error: \(error!)")
             }else{
                 print("Inicio de sesión exitoso")
             }
         }
     }
+    @IBAction func iniciarSesionGoogleTapped(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signIn()
+    }
     
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error == nil{
+            let credential = GoogleAuthProvider.credential(withIDToken: user.authentication.idToken, accessToken: user.authentication.accessToken)
+            Auth.auth().signIn(with: credential){(user,error) in
+                if error != nil{
+                    print("Se presentó el siguiente error: \(String(describing: error))")
+                }else{
+                    print("Inicio de sesión con google exitoso")
+                }
+            }
+            
+        }
+    }
 
 }
 
